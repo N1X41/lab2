e7cc6898c55114a4535260aa81904fff8d2d8987
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:38:00 +0300

    Scripted auto-commit on change (2021-12-24 21:38:00) by gitwatch.sh

--- src/main/java/LAB2/IDKey.java
+++ src/main/java/LAB2/IDKey.java
@@ -4,5 +4,12 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
 public class IDKey implements WritableComparable<IDKey> {
-    
+    private Integer userId;
+    private Text data;
+
+    public IDKey(Integer userId, Text data) {
+        super();
+        this.userId = userId;
+        this.data = data;
+    }
 }
