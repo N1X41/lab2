bad0222bef6b2b86504c0806a90bfd96c5df2812
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:06:26 +0300

    Scripted auto-commit on change (2021-12-24 21:06:26) by gitwatch.sh

--- src/main/java/LAB2/DataJoinApp.java
+++ src/main/java/LAB2/DataJoinApp.java
@@ -15,6 +15,19 @@ public class DataJoinApp {
             System.exit(-1);
         }
 
-        
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
