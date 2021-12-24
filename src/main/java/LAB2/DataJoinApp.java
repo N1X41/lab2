272cdf66f4c88fa1aa776d82e50c7e5817d8b407
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:03:19 +0300

    Scripted auto-commit on change (2021-12-24 21:03:19) by gitwatch.sh

--- src/main/java/LAB2/DataJoinApp.java
+++ src/main/java/LAB2/DataJoinApp.java
@@ -1,15 +1,20 @@
 package LAB2;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.join.CompositeInputFormat;
 
+import java.io.IOException;
 
 public class DataJoinApp {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         if (args.length != 3) {
             System.err.println("Usage: DataJoinApp <input file_1> <input file_2> <output path>");
             System.exit(-1);
         }
 
         JobConf conf = new JobConf(JoinJob.class);
-
+        
     }
 }
