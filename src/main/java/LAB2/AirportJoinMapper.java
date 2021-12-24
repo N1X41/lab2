17a2d2108d248fd31c7c6e94db515fb4af28beae
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:16:50 +0300

    Scripted auto-commit on change (2021-12-24 21:16:50) by gitwatch.sh

--- src/main/java/LAB2/AirportJoinMapper.java
+++ src/main/java/LAB2/AirportJoinMapper.java
@@ -5,7 +5,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
-
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.join.TupleWritable;
 
 public class AirportJoinMapper extends MapReduceBase implements Mapper<LongWritable, TupleWritable, LongWritable, FloatWritable> {
