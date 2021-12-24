a43c821c4dfa60cb51178d0b1f2be0882c92335d
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:13:18 +0300

    Scripted auto-commit on change (2021-12-24 21:13:18) by gitwatch.sh

--- src/main/java/LAB2/DataJoinApp.java
+++ src/main/java/LAB2/DataJoinApp.java
@@ -3,7 +3,8 @@ package LAB2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
