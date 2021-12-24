77c59808cb66e599d47d5b0fd012dc56af217963
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:03:47 +0300

    Scripted auto-commit on change (2021-12-24 21:03:47) by gitwatch.sh

--- src/main/java/LAB2/DataJoinApp.java
+++ src/main/java/LAB2/DataJoinApp.java
@@ -15,6 +15,16 @@ public class DataJoinApp {
         }
 
         JobConf conf = new JobConf(JoinJob.class);
-        
+        conf.setJobName("map join");
+        conf.setInputFormat(CompositeInputFormat.class);
+        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
+        conf.set("mapred.join.expr", CompositeInputFormat.compose("inner",
+                KeyValueTextInputFormat.class,
+                args[0],
+                args[1]));
+        conf.setMapperClass(MapJoinMapper.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(Text.class);
+        JobClient.runJob(conf);
     }
 }
