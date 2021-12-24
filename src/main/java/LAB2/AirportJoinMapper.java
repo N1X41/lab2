a96fc46680499918584d96ca663ad2b108179dda
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:53:12 +0300

    Scripted auto-commit on change (2021-12-24 21:53:11) by gitwatch.sh

--- src/main/java/LAB2/AirportJoinMapper.java
+++ src/main/java/LAB2/AirportJoinMapper.java
@@ -4,9 +4,7 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.join.TupleWritable;
+
 
 import java.io.IOException;
 
