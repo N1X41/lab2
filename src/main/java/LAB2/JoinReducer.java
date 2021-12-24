603f06d644a33adc076eb1b22e5d47c5cc559bb5
N1X41 <nickfirst41@gmail.com>
2021-12-24 22:28:55 +0300

    Scripted auto-commit on change (2021-12-24 22:28:54) by gitwatch.sh

--- src/main/java/LAB2/JoinReducer.java
+++ src/main/java/LAB2/JoinReducer.java
@@ -3,6 +3,8 @@ package LAB2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class JoinReducer extends Reducer<IDKey, String, Text, Text>{
     @Override
     protected void reduce(IDKey key, Iterable<String> values, Context context)
