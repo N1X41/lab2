3475bdb992b5150b57bdd34242614fbf27bee6f2
N1X41 <nickfirst41@gmail.com>
2021-12-25 00:51:26 +0300

    Scripted auto-commit on change (2021-12-25 00:51:26) by gitwatch.sh

--- src/main/java/LAB2/JoinReducer.java
+++ src/main/java/LAB2/JoinReducer.java
@@ -14,7 +14,7 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text>{
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
         while (iter.hasNext()) {
-            float currentTime = Float.parseFloat(cur);
+            float currentTime = Float.parseFloat(iter.next().toString());
             if (currentTime < min) {
                 min = currentTime;
             }
@@ -25,9 +25,8 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text>{
             count++;
             avg /= (float) count;
         }
-        if (key.getFlight()) {
+        if (count > 0) {
             String res = "Name: " + airportName + ", min: " + min + ", max: " + max + ", avg: " + avg;
-            System.out.println(res);
             context.write(new Text(key.getAirportId().toString()), new Text(res));
         }
     }
