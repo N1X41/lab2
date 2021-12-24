8b3906902c059944334460c65c612dd9b54e6df8
N1X41 <nickfirst41@gmail.com>
2021-12-25 00:42:20 +0300

    Scripted auto-commit on change (2021-12-25 00:42:19) by gitwatch.sh

--- src/main/java/LAB2/StringTools.java
+++ src/main/java/LAB2/StringTools.java
@@ -2,4 +2,14 @@ package LAB2;
 
 public class StringTools {
     private static final String DELIMITER = ",";
+
+    public StringTools() {}
+
+    public static String[] splitWithCommas(String columns) {
+        return columns.split(DELIMITER);
+    }
+
+    public static String removeQuotes(String str) {
+        return str.replaceAll("\"", "");
+    }
 }
