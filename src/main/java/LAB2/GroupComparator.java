40b4b90ce24196c6ee74e3aac6491f5f0be4aadd
N1X41 <nickfirst41@gmail.com>
2021-12-24 22:20:08 +0300

    Scripted auto-commit on change (2021-12-24 22:20:08) by gitwatch.sh

--- src/main/java/LAB2/GroupComparator.java
+++ src/main/java/LAB2/GroupComparator.java
@@ -1,9 +1,15 @@
 package LAB2;
 
 import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
 
 public class GroupComparator extends WritableComparator {
     public GroupComparator() {
         super(IDKey.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable){
+        
     }
 }
