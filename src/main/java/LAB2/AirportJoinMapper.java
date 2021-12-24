67337f18d5772d2291b0956f1a21924704032e55
N1X41 <nickfirst41@gmail.com>
2021-12-24 23:01:21 +0300

    Scripted auto-commit on change (2021-12-24 23:01:21) by gitwatch.sh

--- src/main/java/LAB2/AirportJoinMapper.java
+++ src/main/java/LAB2/AirportJoinMapper.java
@@ -15,8 +15,8 @@ public class AirportJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) {
             String[] columns = value.toString().replaceAll(" ", "").split(",");
-            Integer airportCode = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER]);
-            String name = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER].replaceALL(" ", "");
+            Integer airportCode = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER].replaceAll("\"", ""));
+            String name = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER];
             context.write(new IDKey(airportCode, false, name), new Text(name));
         }
     }
