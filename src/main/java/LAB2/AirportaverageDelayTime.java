73381a14e047426ec375636004cbace26f10cd60
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:29:29 +0300

    Scripted auto-commit on change (2021-12-24 21:29:29) by gitwatch.sh

--- src/main/java/LAB2/AirportaverageDelayTime.java
+++ src/main/java/LAB2/AirportaverageDelayTime.java
@@ -2,6 +2,6 @@ package LAB2;
 
 public class AirportaverageDelayTime {
     public abstract class Partitioner<KEY, VALUE> {
-        
+        public abstract int getPartition(KEY key, VALUE value, int numPartitions);
     }
 }
