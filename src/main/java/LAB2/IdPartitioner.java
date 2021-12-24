a4a5d509807ef18132647d67a57c7c4b70cce874
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:42:25 +0300

    Scripted auto-commit on change (2021-12-24 21:42:25) by gitwatch.sh

--- src/main/java/LAB2/IdPartitioner.java
+++ src/main/java/LAB2/IdPartitioner.java
@@ -1,4 +1,11 @@
 package LAB2;
 
-public class IdPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class IdPartitioner extends Partitioner<IDKey, Text> {
+    @Override
+    public int getPartition(IDKey key, Text value, int numPartitions) {
+        return (key.getAirportId().hashCode() & Integer.MAX_VALUE) % numPartitions;
+    }
 }
