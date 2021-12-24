56b2060ec4508fdcb48e379c06d18c541147d954
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:36:36 +0300

    Scripted auto-commit on change (2021-12-24 21:36:36) by gitwatch.sh

--- src/main/java/LAB2/FlightJoinMapper.java
+++ src/main/java/LAB2/FlightJoinMapper.java
@@ -1,6 +1,6 @@
 package LAB2;
 
-import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -9,13 +9,13 @@ import org.apache.hadoop.mapred.join.TupleWritable;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, FloatWritable> {
+public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
     private static final int DELAY_COLUMN_NUMBER = 17;
 
-    public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, IDKey> output, Reporter reporter) throws IOException {
-        IDKey airportCode = (LongWritable) value.get(AIRPORT_CODE_COLUMN_NUMBER);
-        FloatWritable delay = (FloatWritable) value.get(DELAY_COLUMN_NUMBER);
+    public void map(LongWritable key, TupleWritable value, OutputCollector<IDKey, Text> output, Reporter reporter) throws IOException {
+        IDKey airportCode = (IDKey) value.get(AIRPORT_CODE_COLUMN_NUMBER);
+        Text delay = (Text) value.get(DELAY_COLUMN_NUMBER);
         output.collect(airportCode, delay);
     }
 }
