9e93af28129c9236027f8f26aa4ed986e3921b15
N1X41 <nickfirst41@gmail.com>
2021-12-25 00:49:36 +0300

    Scripted auto-commit on change (2021-12-25 00:49:36) by gitwatch.sh

--- src/main/java/LAB2/JoinReducer.java
+++ src/main/java/LAB2/JoinReducer.java
@@ -13,14 +13,8 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text>{
         String airportName = iter.next().toString();
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
-        System.out.println(airportName);
         while (iter.hasNext()) {
-            String cur = iter.next().toString();
-            System.out.println("----------------------");
-            System.out.println(cur);
-            System.out.println("----------------------");
             float currentTime = Float.parseFloat(cur);
-            //float currentTime = Float.parseFloat(iter.next().toString());
             if (currentTime < min) {
                 min = currentTime;
             }
