55ced1f137d05ca6ae969a760d69354d45a8c27c
N1X41 <nickfirst41@gmail.com>
2021-12-24 22:21:44 +0300

    Scripted auto-commit on change (2021-12-24 22:21:44) by gitwatch.sh

--- src/main/java/LAB2/GroupComparator.java
+++ src/main/java/LAB2/GroupComparator.java
@@ -12,5 +12,7 @@ public class GroupComparator extends WritableComparator {
     public int compare(WritableComparable wc1, WritableComparable wc2){
         IDKey ke1 = (IDKey)wc1;
         IDKey ke2 = (IDKey)wc2;
+
+        return 0;
     }
 }
