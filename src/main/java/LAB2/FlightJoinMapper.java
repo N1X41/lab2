802fbacfd53284f7ddc713a7a4a7aea2a731d5a9
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:34:36 +0300

    Scripted auto-commit on change (2021-12-24 21:34:36) by gitwatch.sh

--- src/main/java/LAB2/FlightJoinMapper.java
+++ src/main/java/LAB2/FlightJoinMapper.java
@@ -9,12 +9,12 @@ import org.apache.hadoop.mapred.join.TupleWritable;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, LongWritable, FloatWritable> {
+public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, FloatWritable> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
     private static final int DELAY_COLUMN_NUMBER = 17;
 
-    public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, FloatWritable> output, Reporter reporter) throws IOException {
-        airportCode = (LongWritable) value.get(AIRPORT_CODE_COLUMN_NUMBER);
+    public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, IDKey> output, Reporter reporter) throws IOException {
+        IDKey airportCode = (LongWritable) value.get(AIRPORT_CODE_COLUMN_NUMBER);
         FloatWritable delay = (FloatWritable) value.get(DELAY_COLUMN_NUMBER);
         output.collect(airportCode, delay);
     }
