735783d34cc43609bef4bbd5328a151c9be7cc24
N1X41 <nickfirst41@gmail.com>
2021-12-24 23:02:24 +0300

    Scripted auto-commit on change (2021-12-24 23:02:24) by gitwatch.sh

--- src/main/java/LAB2/JoinReducer.java
+++ src/main/java/LAB2/JoinReducer.java
@@ -11,6 +11,9 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text>{
     protected void reduce(IDKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
         Iterator<Text> iter = values.iterator();
         String name;
+        if (iter.hasNext()) {
+            name = iter.next().toString();
+        }
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
         while (iter.hasNext()) {
