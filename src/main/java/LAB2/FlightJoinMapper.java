a6d0f477e762f84fcb8a8c63a211d0da9199364e
N1X41 <nickfirst41@gmail.com>
2021-12-24 21:46:19 +0300

    Scripted auto-commit on change (2021-12-24 21:46:19) by gitwatch.sh

--- src/main/java/LAB2/FlightJoinMapper.java
+++ src/main/java/LAB2/FlightJoinMapper.java
@@ -9,28 +9,9 @@ import org.apache.hadoop.mapred.join.TupleWritable;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, Text> {
+public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
     private static final int DELAY_COLUMN_NUMBER = 17;
 
-    public void map(LongWritable key, TupleWritable value, OutputCollector<IDKey, Text> output, Reporter reporter) throws IOException {
-        IDKey airportCode = (IDKey) value.get(AIRPORT_CODE_COLUMN_NUMBER);
-        Text delay = (Text) value.get(DELAY_COLUMN_NUMBER);
-        output.collect(airportCode, delay);
-    }
-
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-    }
-
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        Pattern ptr = Pattern.compile(PATTERN);
-        Matcher matcher = ptr.matcher(line);
-        while (matcher.find()) {
-            context.write(new Text(matcher.group().toLowerCase()), new IntWritable(1));
-        }
-    }
+    
 }
