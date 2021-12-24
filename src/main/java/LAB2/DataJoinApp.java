5fee6af714ae347ec82d365c8f2181d7638b7349
N1X41 <nickfirst41@gmail.com>
2021-12-24 20:50:17 +0300

    Scripted auto-commit on change (2021-12-24 20:50:17) by gitwatch.sh

--- src/main/java/LAB2/DataJoinApp.java
+++ src/main/java/LAB2/DataJoinApp.java
@@ -2,6 +2,9 @@ package LAB2;
 
 public class DataJoinApp {
     public static void main(String[] args){
-
+        if (args.length != 3) {
+            System.err.println("Usage: DataJoinApp <input file_1> <input file_2> <output path>");
+            System.exit(-1);
+        }
     }
 }
