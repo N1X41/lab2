67a566fc00ab957c6ce55fa315f4ea1e2b14efb1
N1X41 <nickfirst41@gmail.com>
2022-02-11 19:48:11 +0300

    Scripted auto-commit on change (2022-02-11 19:48:11) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -6,13 +6,8 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.broadcast.Broadcast;
-import scala.Predef;
 import scala.Tuple2;
 
-import java.util.Map;
 
 public class FlightApp {
     public static void main(String[] args) {
