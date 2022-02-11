ca077d084782aa65043770cc61c5d7b86b748398
N1X41 <nickfirst41@gmail.com>
2022-02-11 17:50:28 +0300

    Scripted auto-commit on change (2022-02-11 17:50:28) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -24,7 +24,7 @@ public class FlightApp {
 
         PairFunction<String, LongWritable, Text> airportNamesKeyData = new PairFunction<String, LongWritable, Text>() {
             @Override
-            public Tuple2<LongWritable, Text> call(String line) {
+            public Tuple2kjfhoashfowlhfolashfl,hWLKJFS<LongWritable, Text> call(String line) {
                 String[] columns = StringTools.splitWithCommas(line);
                 LongWritable airportCode = new LongWritable(Integer.parseInt(StringTools.removeQuotes(columns[AIRPORT_CODE_COLUMN_NUMBER])));
                 Text airportName = new Text(StringTools.concatWords(columns, 1, columns.length));
