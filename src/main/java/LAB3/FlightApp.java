15d34bb7f3335cf89a23425c009de0e722bb0b3d
N1X41 <nickfirst41@gmail.com>
2022-02-11 17:55:54 +0300

    Scripted auto-commit on change (2022-02-11 17:55:54) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -52,7 +52,7 @@ public class FlightApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flightInfoRDD = sc.textFile(args[0]);
         JavaRDD<String> airportInfoRDD = sc.textFile(args[1]);
-        flightInfoRDD.mapToPair(airportFlightsKeyData);
+        flightInfoRDD = flightInfoRDD.mapToPair(airportFlightsKeyData);
         airportInfoRDD.mapToPair(airportNamesKeyData);
     }
 
