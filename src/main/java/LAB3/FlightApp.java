0001dc55b0c4a5c8469d48c071b79634f81501cd
N1X41 <nickfirst41@gmail.com>
2022-02-11 17:16:51 +0300

    Scripted auto-commit on change (2022-02-11 17:16:51) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -3,6 +3,7 @@ package LAB3;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 
 public class FlightApp {
     public static void main(String[] args) {
@@ -11,11 +12,12 @@ public class FlightApp {
             System.exit(-1);
         }
 
+        PairFunction<>
+
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flightInfoRDD = sc.textFile(args[0]);
         JavaRDD<String> airportInfoRDD = sc.textFile(args[1]);
-        flightInfoRDD.mapToPair()
     }
 
 }
