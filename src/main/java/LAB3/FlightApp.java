381b7e4b33db8fa0b66bb71aa29dfbe4415faa06
N1X41 <nickfirst41@gmail.com>
2022-02-11 21:24:47 +0300

    Scripted auto-commit on change (2022-02-11 21:24:47) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -28,7 +28,7 @@ public class FlightApp {
                 .mapToPair(AirportSparkFunctions.airportNamesKeyData);
         JavaPairRDD<Tuple2<Long, Long> ,FlightData> reducedFlightInfo = flightInfoPairRDD
                 .reduceByKey(AirportSparkFunctions.airportFlightsUniqueKeyData);
-        
+        Broadcast<Map<Long, String>> airportInfoBroadcasted
         JavaPairRDD<String, String> result = reducedFlightInfo
                 .mapToPair(AirportSparkFunctions.getAirportResultData(sc.broadcast(airportInfoPairRDD.collectAsMap())));
         result.saveAsTextFile(args[2]);
