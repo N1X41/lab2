596cf0dee5a23a32025076173b44746f973c0f3a
N1X41 <nickfirst41@gmail.com>
2022-02-11 19:39:11 +0300

    Scripted auto-commit on change (2022-02-11 19:39:11) by gitwatch.sh

--- src/main/java/LAB3/AirportSparkFunctions.java
+++ src/main/java/LAB3/AirportSparkFunctions.java
@@ -2,10 +2,13 @@ package LAB3;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 public class AirportSparkFunctions {
+
+
     PairFunction<String, LongWritable, Text> airportNamesKeyData = new PairFunction<String, LongWritable, Text>() {
         @Override
         public Tuple2<LongWritable, Text> call(String line) {
