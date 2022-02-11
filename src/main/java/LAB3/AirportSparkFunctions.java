e4fb64c5c4c351516cb2647d42ecb633f36c5995
N1X41 <nickfirst41@gmail.com>
2022-02-11 21:53:39 +0300

    Scripted auto-commit on change (2022-02-11 21:53:39) by gitwatch.sh

--- src/main/java/LAB3/AirportSparkFunctions.java
+++ src/main/java/LAB3/AirportSparkFunctions.java
@@ -58,14 +58,12 @@ public class AirportSparkFunctions {
             };
 
     public static PairFunction<Tuple2<Tuple2<Long, Long>, FlightData>, String, String>
-        getAirportResultData (Broadcast<Map<Long, String>> airportInfoBroadcasted){
-        return new PairFunction<Tuple2<Tuple2<Long, Long>, FlightData>, String, String>() {
-            @Override
-            public Tuple2<String, String> call(Tuple2<Tuple2<Long, Long>, FlightData> e) {
-                String originName = airportInfoBroadcasted.value().get(e._1._1);
-                String destName = airportInfoBroadcasted.value().get(e._1._2);
-                String key = originName + " -> " + destName;
-                String value = String.format("Delay: %f, Ratio: %.2f%%", e._2.getDelay(), e._2.getRatio());
+    getFlightResultData(Broadcast<Map<Long, String>> airportInfoBroadcasted) {
+        return (PairFunction<Tuple2<Tuple2<Long, Long>, FlightData>, String, String>) e -> {
+            String originName = airportInfoBroadcasted.value().get(e._1._1);
+            String destName = airportInfoBroadcasted.value().get(e._1._2);
+            String key = originName + " -> " + destName;
+            String value = String.format("Delay: %f, Ratio: %.2f%%", e._2.getDelay(), e._2.getRatio());
 
                 return new Tuple2<>(key, value);
             }
