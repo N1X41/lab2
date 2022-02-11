3ac34c17f36b35fd2680cc4be1904bd066503cdc
N1X41 <nickfirst41@gmail.com>
2022-02-11 17:17:44 +0300

    Scripted auto-commit on change (2022-02-11 17:17:44) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -13,7 +13,7 @@ public class FlightApp {
             System.exit(-1);
         }
 
-        PairFunction<LongWritable, Text> airportNamesKeyData
+        PairFunction<LongWritable, Text> airportNamesKeyData =
 
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
