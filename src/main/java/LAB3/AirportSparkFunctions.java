3885b9c606a5b0430a71d032932f9dfc7fde8838
N1X41 <nickfirst41@gmail.com>
2022-02-11 19:38:43 +0300

    Scripted auto-commit on change (2022-02-11 19:38:43) by gitwatch.sh

--- src/main/java/LAB3/AirportSparkFunctions.java
+++ src/main/java/LAB3/AirportSparkFunctions.java
@@ -6,4 +6,13 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 public class AirportSparkFunctions {
+    PairFunction<String, LongWritable, Text> airportNamesKeyData = new PairFunction<String, LongWritable, Text>() {
+        @Override
+        public Tuple2<LongWritable, Text> call(String line) {
+            String[] columns = StringTools.splitWithCommas(line);
+            LongWritable airportCode = new LongWritable(Integer.parseInt(StringTools.removeQuotes(columns[AIRPORT_CODE_COLUMN_NUMBER])));
+            Text airportName = new Text(StringTools.concatWords(columns, 1, columns.length));
+            return new Tuple2<>(airportCode, airportName);
+        }
+    };
 }
