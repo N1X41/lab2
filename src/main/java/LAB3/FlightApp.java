55b4357bea5ba7d5f51ac550923b9c787a1cc85b
N1X41 <nickfirst41@gmail.com>
2022-02-11 13:12:51 +0300

    Scripted auto-commit on change (2022-02-11 13:12:51) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -1,4 +1,11 @@
 package LAB3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class FlightApp {
+    public static void main(String[] args) {
+        
+    }
 }
