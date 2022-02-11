c4b556f29d0f48e130100af526b79a7283558d9d
N1X41 <nickfirst41@gmail.com>
2022-02-11 19:30:31 +0300

    Scripted auto-commit on change (2022-02-11 19:30:31) by gitwatch.sh

--- src/main/java/LAB3/FlightApp.java
+++ src/main/java/LAB3/FlightApp.java
@@ -52,7 +52,7 @@ public class FlightApp {
             }
         };
 
-        PairFunction<Tuple2<Tuple2<LongWritable, LongWritable>, FlightData>, String, String> airportResultData = new PairFunction<Tuple2<LongWritable, LongWritable>, String, String>() {
+        PairFunction<Tuple2<Tuple2<LongWritable, LongWritable>, FlightData>, String, String> airportResultData = new PairFunction<Tuple2<Tuple2<LongWritable, LongWritable>, FlightData>, String, String>() {
             @Override
             public Tuple2<String, String> call(Tuple2<LongWritable, LongWritable> e){
                 return null;
