d7af178fdd226dc160a05bbe35c5d057c2e4151e
N1X41 <nickfirst41@gmail.com>
2022-02-11 21:53:28 +0300

    Scripted auto-commit on change (2022-02-11 21:53:28) by gitwatch.sh

--- src/main/java/LAB3/AirportSparkFunctions.java
+++ src/main/java/LAB3/AirportSparkFunctions.java
@@ -54,9 +54,8 @@ public class AirportSparkFunctions {
                 int dfc1 = fd1.getDelayedFlightCount(), dfc2 = fd2.getDelayedFlightCount();
                 int fc1 = fd1.getFlightCount(), fc2 = fd2.getFlightCount();
 
-            return new FlightData(newDelay, afc1 + afc2, dfc1 + dfc2, fc1 + fc2);
-        }
-    };
+                return new FlightData(newDelay, afc1 + afc2, dfc1 + dfc2, fc1 + fc2);
+            };
 
     public static PairFunction<Tuple2<Tuple2<Long, Long>, FlightData>, String, String>
         getAirportResultData (Broadcast<Map<Long, String>> airportInfoBroadcasted){
