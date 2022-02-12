ce8813b590cc7d38acfcbb267631613451e2e70c
N1X41 <nickfirst41@gmail.com>
2022-02-12 21:10:20 +0300

    Scripted auto-commit on change (2022-02-12 21:10:20) by gitwatch.sh

--- src/main/java/LAB4/StoreActor.java
+++ src/main/java/LAB4/StoreActor.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 public class StoreActor extends AbstractActor {
     private HashMap<String, TestResults> storage;
+    private static final String RUN_ACTOR_START_MSG_FORMAT = "PackageID: %s. Message: Starting running tests!\n";
 
     public StoreActor() {
         storage = new HashMap<>();
