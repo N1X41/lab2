56c4915b4f38f2501df46ddd45c99a62f6cd2e8c
N1X41 <nickfirst41@gmail.com>
2022-02-12 19:00:46 +0300

    Scripted auto-commit on change (2022-02-12 19:00:46) by gitwatch.sh

--- src/main/java/LAB4/StoreMessage.java
+++ src/main/java/LAB4/StoreMessage.java
@@ -3,4 +3,11 @@ package LAB4;
 public class StoreMessage {
     private String packageID;
     private String functionName;
+    private TestResults results;
+
+    public StoreMessage (String packageID, String functionName, TestResults results) {
+        this.packageID = packageID;
+        this.functionName = functionName;
+        this.results = results;
+    }
 }
