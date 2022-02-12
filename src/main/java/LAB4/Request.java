d4980a534ecf39634f67566a58fff6bc2eb2dcf3
N1X41 <nickfirst41@gmail.com>
2022-02-12 15:04:51 +0300

    Scripted auto-commit on change (2022-02-12 15:04:51) by gitwatch.sh

--- src/main/java/LAB4/Request.java
+++ src/main/java/LAB4/Request.java
@@ -1,4 +1,16 @@
 package LAB4;
 
 public class Request {
+    private String packageId;
+    private String jsScript;
+    private String functionName;
+    private Test[] tests;
+
+    public Request (String packageId, String jsScript, String functionName, Test[] tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
 }
