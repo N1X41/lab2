24fff1506c195cb9b2a50b8fe7626dad36729d3e
N1X41 <nickfirst41@gmail.com>
2022-02-12 14:48:50 +0300

    Scripted auto-commit on change (2022-02-12 14:48:50) by gitwatch.sh

--- src/main/java/LAB4/StoreActor.java
+++ src/main/java/LAB4/StoreActor.java
@@ -1,4 +1,11 @@
 package LAB4;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+
+public class StoreActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
