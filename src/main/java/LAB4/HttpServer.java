aca23f8fe2ee43c656daa96b4a4b1d10ea6bae78
N1X41 <nickfirst41@gmail.com>
2022-02-12 17:25:21 +0300

    Scripted auto-commit on change (2022-02-12 17:25:20) by gitwatch.sh

--- src/main/java/LAB4/HttpServer.java
+++ src/main/java/LAB4/HttpServer.java
@@ -6,7 +6,7 @@ import akka.actor.Props;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.StatusCode;
 import akka.http.javadsl.server.Route;
-
+import akka.http.scaladsl.server.PathMatchers;
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.http.javadsl.server.Directives.entity;
@@ -34,7 +34,7 @@ public class HttpServer {
                                                     file.getTests()), ActorRef.noSender());
                                             return complete(StatusCode.OK, String.format(PACKAGE_TEST_START_FORMAT, file.getPackageId()));
                                         })))),
-                path ("result" / id)
+                path ("result" / PathMatchers.IntNumber$)
         );
     }
 }
