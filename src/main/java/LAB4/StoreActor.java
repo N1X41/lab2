1d062c5b8d1291147c480f3d8fbe21ed9a2aa8cb
N1X41 <nickfirst41@gmail.com>
2022-02-12 19:19:00 +0300

    Scripted auto-commit on change (2022-02-12 19:19:00) by gitwatch.sh

--- src/main/java/LAB4/StoreActor.java
+++ src/main/java/LAB4/StoreActor.java
@@ -3,7 +3,10 @@ package LAB4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+
 public class StoreActor extends AbstractActor {
+    private HashMap<>
 
     @Override
     public Receive createReceive() {
