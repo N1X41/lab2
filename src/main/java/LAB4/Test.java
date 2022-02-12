e66a0be064e451d21ca02331063174ceb4769492
N1X41 <nickfirst41@gmail.com>
2022-02-12 15:03:01 +0300

    Scripted auto-commit on change (2022-02-12 15:03:01) by gitwatch.sh

--- src/main/java/LAB4/Test.java
+++ src/main/java/LAB4/Test.java
@@ -4,4 +4,22 @@ public class Test {
     private String testName;
     private String expectedResult;
     private Object[] params;
+
+    public Test(String testName, String expectedResult, Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
 }
