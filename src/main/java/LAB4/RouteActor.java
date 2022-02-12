6fae6709cb25e7a3b3e9bcacd8222436137aab06
N1X41 <nickfirst41@gmail.com>
2022-02-12 15:13:07 +0300

    Scripted auto-commit on change (2022-02-12 15:13:07) by gitwatch.sh

--- src/main/java/LAB4/RouteActor.java
+++ src/main/java/LAB4/RouteActor.java
@@ -16,7 +16,7 @@ public class RouteActor extends AbstractActor {
                 path ( "execute", () ->
                         route (
                                 post( () ->
-                                        entity()
+                                        entity(Jackson.unmarshaller())
                                 )
                         )
                 )
