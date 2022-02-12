c393b251f95feb58079522e981796c00ca66ef29
N1X41 <nickfirst41@gmail.com>
2022-02-12 18:00:42 +0300

    Scripted auto-commit on change (2022-02-12 18:00:42) by gitwatch.sh

--- src/main/java/LAB4/TestResults.java
+++ src/main/java/LAB4/TestResults.java
@@ -1,4 +1,17 @@
 package LAB4;
 
+import java.util.HashMap;
+
 public class TestResults {
+    private HashMap<String, Boolean> results;
+
+    public TestResults(){
+        results = new HashMap<>();
+    }
+
+    public void runTests(Test[] tests) throws Exception{
+        for (Test test : tests){
+            
+        }
+    }
 }
