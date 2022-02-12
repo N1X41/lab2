73a90ff956421ce683f88143380fa3a9e359ef11
N1X41 <nickfirst41@gmail.com>
2022-02-12 17:23:31 +0300

    Scripted auto-commit on change (2022-02-12 17:23:31) by gitwatch.sh

--- src/main/java/LAB4/HttpServer.java
+++ src/main/java/LAB4/HttpServer.java
@@ -32,7 +32,7 @@ public class HttpServer {
                                                     file.getFunctionName(),
                                                     file.getTests()), ActorRef.noSender());
                                             return complete(StatusCode.OK, String.format(PACKAGE_TEST_START_FORMAT, file.getPackageId()));
-                                        }))))
+                                        })))),
         );
     }
 }
