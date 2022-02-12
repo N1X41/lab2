f0fe00ecddbc089b838c65badb2acc7a883f6faa
N1X41 <nickfirst41@gmail.com>
2022-02-12 15:06:11 +0300

    Scripted auto-commit on change (2022-02-12 15:06:11) by gitwatch.sh

--- src/main/java/LAB4/TestingApp.java
+++ src/main/java/LAB4/TestingApp.java
@@ -8,6 +8,8 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class TestingApp {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("Testing");
@@ -18,6 +20,6 @@ public class TestingApp {
     }
 
     public void createRoute(){
-        
+
     }
 }
