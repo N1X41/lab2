82aee959cca672fbf7434428f7f5f37e8d53e687
N1X41 <nickfirst41@gmail.com>
2022-02-12 14:47:46 +0300

    Scripted auto-commit on change (2022-02-12 14:47:46) by gitwatch.sh

--- src/main/java/LAB4/HttpServer.java
+++ src/main/java/LAB4/HttpServer.java
@@ -3,8 +3,8 @@ package LAB4;
 
 import akka.actor.ActorSystem;
 
-public class Server {
-    public Server(ActorSystem system) {
+public class HttpServer {
+    public HttpServer(ActorSystem system) {
 
     }
 }
