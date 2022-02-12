9e41d798747364523303705331a024486cca5460
N1X41 <nickfirst41@gmail.com>
2022-02-12 18:16:23 +0300

    Scripted auto-commit on change (2022-02-12 18:16:23) by gitwatch.sh

--- src/main/java/LAB4/TestResults.java
+++ src/main/java/LAB4/TestResults.java
@@ -17,19 +17,22 @@ public class TestResults {
     }
 
     public void runTests(Test[] tests, String jsCode, String functionName, String packageID) throws Exception{
-        for (Test test : tests){
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
-            engine.eval(jsCode);
-            Invocable invocable = (Invocable)engine;
-            String result = invocable.invokeFunction(functionName, test.getParams()).toString();
-            boolean isRight = result.equals(test.getExpectedResult());
-            if (isRight) {
-                System.out.printf(SUCCES_TEST_FORMAT,packageID, test.getTestName());
-            } else {
-                System.out.printf(FAIL_TEST_FORMAT,packageID, test.getTestName(), test.getExpectedResult(), result);
-
+        try {
+            for (Test test : tests) {
+                ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
+                engine.eval(jsCode);
+                Invocable invocable = (Invocable) engine;
+                String result = invocable.invokeFunction(functionName, test.getParams()).toString();
+                boolean isRight = result.equals(test.getExpectedResult());
+                if (isRight) {
+                    System.out.printf(SUCCES_TEST_FORMAT, packageID, test.getTestName());
+                } else {
+                    System.out.printf(FAIL_TEST_FORMAT, packageID, test.getTestName(), test.getExpectedResult(), result);
+                }
+                results.put(test.getTestName(), isRight);
             }
-            results.put(test.getTestName(), isRight);
+        } catch (Exception e){
+            
         }
     }
 
