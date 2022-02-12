336ee06b164c286b7222e35b27ecc8e45bca5748
N1X41 <nickfirst41@gmail.com>
2022-02-12 16:59:20 +0300

    Scripted auto-commit on change (2022-02-12 16:59:20) by gitwatch.sh

--- src/main/java/LAB4/RouteActor.java
+++ src/main/java/LAB4/RouteActor.java
@@ -4,6 +4,7 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 import akka.stream.javadsl.Balance;
+import akka.routing.BalancingPool;
 
 public class RouteActor extends AbstractActor {
     private final static int POOL_SIZE = 2;
@@ -13,7 +14,7 @@ public class RouteActor extends AbstractActor {
 
     @Override
     public void preStart(){
-        balanceActor = getContext().actorOf(new Balance)
+        balanceActor = getContext().actorOf(new BalancingPool())
     }
 
     @Override
