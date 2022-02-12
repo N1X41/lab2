079834349f843c86c15f5fdc9098441818842214
N1X41 <nickfirst41@gmail.com>
2022-02-12 16:44:31 +0300

    Scripted auto-commit on change (2022-02-12 16:44:31) by gitwatch.sh

--- src/main/java/LAB4/RouteActor.java
+++ src/main/java/LAB4/RouteActor.java
@@ -8,10 +8,6 @@ import static akka.http.javadsl.server.Directives.*;
 public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
-    }
-
-    public void createRoute() {
         route (
                 path ( "run", () ->
                         route (
