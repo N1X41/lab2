a7dea42bbf37d432252935cb4362b07fe1e92f43
N1X41 <nickfirst41@gmail.com>
2022-02-12 20:53:41 +0300

    Scripted auto-commit on change (2022-02-12 20:53:41) by gitwatch.sh

--- src/main/java/LAB4/JsonFile.java
+++ src/main/java/LAB4/JsonFile.java
@@ -19,10 +19,12 @@ public class JsonFile {
     }
 
     public String getPackageId() {
+
         return packageID;
     }
 
     public String getJsScript() {
+
         return jsScript;
     }
 
@@ -31,22 +33,27 @@ public class JsonFile {
     }
 
     public Test[] getTests() {
+
         return tests;
     }
 
     public void setPackageId(String packageId) {
+
         this.packageID = packageId;
     }
 
     public void setFunctionName(String functionName) {
+
         this.functionName = functionName;
     }
 
     public void setJsScript(String jsScript) {
+
         this.jsScript = jsScript;
     }
 
     public void setTests(Test[] tests) {
+
         this.tests = tests;
     }
 }
