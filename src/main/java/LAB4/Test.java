a2200c74527c1ff552abaed84c514a991e39655e
N1X41 <nickfirst41@gmail.com>
2022-02-12 17:36:25 +0300

    Scripted auto-commit on change (2022-02-12 17:36:25) by gitwatch.sh

--- src/main/java/LAB4/Test.java
+++ src/main/java/LAB4/Test.java
@@ -26,8 +26,4 @@ public class Test {
     public Object[] getParams() {
         return params;
     }
-
-    public void runTest() throws Exception {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
-    }
 }
