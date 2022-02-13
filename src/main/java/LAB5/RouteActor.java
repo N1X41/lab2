edbadb838dae69d23d271c880a27233f561f56d3
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:03:44 +0300

    Scripted auto-commit on change (2022-02-13 15:03:44) by gitwatch.sh

--- src/main/java/LAB5/RouteActor.java
+++ src/main/java/LAB5/RouteActor.java
@@ -1,4 +1,11 @@
 package LAB5;
 
-public class RouteActor {
+import akka.actor.AbstractActor;
+
+public class RouteActor extends AbstractActor{
+
+    @Override
+    public Receive createReceive(){
+        return null;
+    }
 }
