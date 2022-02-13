fdb61602b0baf1079fee11ffa9bab94b63799a6d
N1X41 <nickfirst41@gmail.com>
2022-02-13 20:57:35 +0300

    Scripted auto-commit on change (2022-02-13 20:57:35) by gitwatch.sh

--- src/main/java/LAB6/ZooKeeperConn.java
+++ src/main/java/LAB6/ZooKeeperConn.java
@@ -4,6 +4,7 @@ import org.apache.zookeeper.ZooKeeper;
 
 public class ZooKeeperConn {
     private static final String HOST = "localhost:2181";
+    private ZooKeeper keeper;
 
     public ZooKeeperConn() {
     }
