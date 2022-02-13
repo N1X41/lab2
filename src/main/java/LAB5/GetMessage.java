e901d03e04dc6e75e4cf9b3a928a9025713af69a
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:01:06 +0300

    Scripted auto-commit on change (2022-02-13 15:01:06) by gitwatch.sh

--- src/main/java/LAB5/GetMessage.java
+++ src/main/java/LAB5/GetMessage.java
@@ -1,4 +1,13 @@
 package LAB5;
 
 public class GetMessage {
+    private String url;
+
+    public GetMessage(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
