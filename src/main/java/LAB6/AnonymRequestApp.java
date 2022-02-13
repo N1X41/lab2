3c4a3d35cfc55668336e630d87ae051359048a5c
N1X41 <nickfirst41@gmail.com>
2022-02-13 19:40:39 +0300

    Scripted auto-commit on change (2022-02-13 19:40:39) by gitwatch.sh

--- src/main/java/LAB6/AnonymRequestApp.java
+++ src/main/java/LAB6/AnonymRequestApp.java
@@ -3,6 +3,7 @@ package LAB6;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
