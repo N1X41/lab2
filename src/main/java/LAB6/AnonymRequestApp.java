4564c499e4e852d94b94e7414529b06ee2a47f9f
N1X41 <nickfirst41@gmail.com>
2022-02-13 19:20:24 +0300

    Scripted auto-commit on change (2022-02-13 19:20:24) by gitwatch.sh

--- src/main/java/LAB6/AnonymRequestApp.java
+++ src/main/java/LAB6/AnonymRequestApp.java
@@ -4,5 +4,5 @@ import java.util.logging.Logger;
 
 public class AnonymRequestApp {
     private final static String PATH_TO_LOG_FILE = "/home/nick/gitwatch/lab2/logs/lab6.log";
-    public final static Logger LOGGER = Logger.getLogger("lab6");
+    public final static Logger LOGGER = Logger.getLogger("MyLog");
 }
