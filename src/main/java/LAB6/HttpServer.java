238ddf8138f4be101a338ebbaaad2e1ccbee2340
N1X41 <nickfirst41@gmail.com>
2022-02-13 20:16:38 +0300

    Scripted auto-commit on change (2022-02-13 20:16:38) by gitwatch.sh

--- src/main/java/LAB6/HttpServer.java
+++ src/main/java/LAB6/HttpServer.java
@@ -26,7 +26,7 @@ public class HttpServer {
                             if (Integer.parseInt(count) <= 0){
                                 return completeWithFuture(http.singleRequest(HttpRequest.create(url)));
                             }
-                            return completeWithFuture(Patterns.ask(actor, new Server()))
+                            return completeWithFuture(Patterns.ask(actor, new Server(url)))
                         }))));
     }
 }
