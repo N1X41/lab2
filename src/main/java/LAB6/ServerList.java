4bb9db733496e2907546365a04115ee7755372a8
N1X41 <nickfirst41@gmail.com>
2022-02-13 19:45:39 +0300

    Scripted auto-commit on change (2022-02-13 19:45:39) by gitwatch.sh

--- src/main/java/LAB6/ServerList.java
+++ src/main/java/LAB6/ServerList.java
@@ -1,4 +1,15 @@
 package LAB6;
 
+
+
 public class ServerList {
+    private final List<String> servers;
+
+    public ServersList(List<String> servers) {
+        this.servers = servers;
+    }
+
+    public List<String> getServers() {
+        return servers;
+    }
 }
