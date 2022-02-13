88e6c6797824d239fe770e0ca688d4b452277e58
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:29:29 +0300

    Scripted auto-commit on change (2022-02-13 15:29:29) by gitwatch.sh

--- src/main/java/LAB5/HttpServer.java
+++ src/main/java/LAB5/HttpServer.java
@@ -49,7 +49,7 @@ public class HttpServer {
                                 .mapConcat(p -> new ArrayList<>(Collections.nCopies(p.second(), p.first())))
                                 .mapAsync(r.second(), url -> {
                                     long start = System.currentTimeMillis();
-                                    asyncHttpClient()
+                                    asyncHttpClient().prepareGet(url).execute();
                                     long finish = System.currentTimeMillis();
                                     return CompletableFuture.completedFuture((int) (finish - start));
                                 });
