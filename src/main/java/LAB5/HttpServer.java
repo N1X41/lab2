abd68fa5db859596b00a16e51b571397c967e373
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:14:30 +0300

    Scripted auto-commit on change (2022-02-13 15:14:30) by gitwatch.sh

--- src/main/java/LAB5/HttpServer.java
+++ src/main/java/LAB5/HttpServer.java
@@ -11,7 +11,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class HttpServer {
-    private static final String URL_ARG = "testU";
+    private static final String URL_ARG = "testUrl";
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system,
                                                                       ActorMaterializer materializer, ActorRef actor) {
