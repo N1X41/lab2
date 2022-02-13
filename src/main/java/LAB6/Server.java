d9d47fac401476f5cb3fb33430b7e8d6eb6f5cc6
N1X41 <nickfirst41@gmail.com>
2022-02-13 19:48:15 +0300

    Scripted auto-commit on change (2022-02-13 19:48:15) by gitwatch.sh

--- src/main/java/LAB6/Server.java
+++ src/main/java/LAB6/Server.java
@@ -3,5 +3,11 @@ package LAB6;
 public class Server {
     private String url;
 
-    
+    public Server(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
