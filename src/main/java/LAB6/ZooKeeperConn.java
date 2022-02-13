284f7ca67dae1c94bd3be5a3de9c28ffb94c3112
N1X41 <nickfirst41@gmail.com>
2022-02-13 20:59:41 +0300

    Scripted auto-commit on change (2022-02-13 20:59:41) by gitwatch.sh

--- src/main/java/LAB6/ZooKeeperConn.java
+++ src/main/java/LAB6/ZooKeeperConn.java
@@ -9,6 +9,16 @@ public class ZooKeeperConn {
     private static final String HOST = "localhost:2181";
     private ZooKeeper keeper;
 
-    public ZooKeeperConn() {
+    public static Watcher watcher = watchedEvent -> {
+
+    };
+
+    public ZooKeeperConn() throws IOException, KeeperException, InterruptedException {
+        keeper = new ZooKeeper(HOST, (int)Duration.ofSeconds(5).getSeconds() * 1000, watcher);
+        keeper.create("/servers/" + AnonymRequestsApp.PORT, (AnonymRequestsApp.PORT + "").getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        WatchedEvent event = new WatchedEvent(Watcher.Event.EventType.NodeCreated, Watcher.Event.KeeperState.SyncConnected, "");
+        watcher.process(event);
+
     }
 }
