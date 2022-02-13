f8af7368aee72ef5618a702aaf1eb7053a0f47d1
N1X41 <nickfirst41@gmail.com>
2022-02-13 14:55:29 +0300

    Scripted auto-commit on change (2022-02-13 14:55:29) by gitwatch.sh

--- src/main/java/LAB5/CasheTestingApp.java
+++ src/main/java/LAB5/CasheTestingApp.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 
 public class CasheTestingApp {
     private static final String PATH_TO_LOG_FILE = "/home/nick/gitwatch/lab2/logs/log5.log";
+    public final static Logger LOGGER = Logger.getLogger("MyLog");
 
     public static void main(String[] args) throws Exception {
 
