8416b5780c4f49f4e1615d3c6f5ef4c5a98a83a1
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:00:32 +0300

    Scripted auto-commit on change (2022-02-13 15:00:31) by gitwatch.sh

--- src/main/java/LAB5/StoreMessage.java
+++ src/main/java/LAB5/StoreMessage.java
@@ -3,4 +3,17 @@ package LAB5;
 public class StoreMessage {
     private String url;
     private int time;
+
+    public StoreMessage(String url, int time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
