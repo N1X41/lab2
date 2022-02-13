bfbdd3c967aa2627676fdea89eea8fea1ded3b0b
N1X41 <nickfirst41@gmail.com>
2022-02-13 15:24:14 +0300

    Scripted auto-commit on change (2022-02-13 15:24:14) by gitwatch.sh

--- src/main/java/LAB5/HttpServer.java
+++ src/main/java/LAB5/HttpServer.java
@@ -39,10 +39,10 @@ public class HttpServer {
                 .mapAsync(MAP_ASYNC, r ->{
                     CompletionStage<Object> stage = Patterns.ask(actor, new GetMessage(r.first()), TIMEOUT);
                     return stage.thenCompose(res -> {
-                        if ((int)res >= 0 {
+                        if ((int)res >= 0) {
                             return CompletableFuture.completedFuture(new Pair<>(r.first(), ((int)res)));
                     }
-                    Flow<Pair<String, Integer>, Integer, NotUsed> flow = Flow<Pair<String, Ineger>>
+                    Flow<Pair<String, Integer>, Integer, NotUsed> flow = Flow<Pair<String, Integer>>
                 })
     }
 }
